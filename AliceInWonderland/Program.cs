2d8b884e55a9d2c060b22340de5d6e2a3503216e
Scott Kuhl <scott@kuhl.ws>
2018-09-02 15:59:26 -0500

    Add project files.

---
+++ AliceInWonderland/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AliceInWonderland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
